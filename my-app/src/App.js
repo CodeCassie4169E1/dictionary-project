43fc92d4fd8601f370a70fbdc9d2de58596ae20f
CodeCassie4169E1 <clipcassie@yahoo.com>
2025-04-01 10:59:34 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="{logo}" className="App-logo-img" alt="logo" />
+      </header>
+      Hello
+    </div>
+  );
+}
+
+export default App;
